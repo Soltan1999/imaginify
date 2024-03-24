f581d15f44ed3d44227c6d905554d1a534b834f9
Soltan1999 <soltanabdurahmanov99@gmail.com>
2024-03-24 23:23:05 +0400

    sss

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-prototype-builtins */ //soltannnnnnnnn
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
